fbf93e98c60be3f8fdb7f399efd0631dfc123bd1
Fors1t-g <vovas64521@Mail.ru>
2025-09-06 00:45:27 +0300

    fix

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -4,12 +4,13 @@ package koschei.models;
 public class Island2 {
 
     private Wood3 wood;
+
     public Island2(Wood3 wood) {
         this.wood = wood;
     }
 
     @Override
     public String toString() {
-        return ", на острове дерево " + wood.toString();
+        return " , на острове дерево " + wood.toString();
     }
 }
